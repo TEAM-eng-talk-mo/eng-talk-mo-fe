a47935150efedd7e86734d24e265e622f5987a54
lurgi <wahoo9040@naver.com>
2024-02-08 20:30:21 +0900

    feat(Home UI): slider pagenation

--- components/StudyList/OnlineStudyList.tsx
+++ components/StudyList/OnlineStudyList.tsx
@@ -1,11 +1,47 @@
 "use client";
-import { useEffect, useState } from "react";
-import { A11y, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useRef, useState } from "react";
+
+import { A11y } from "swiper/modules";
+import { Swiper as SwiperContianer, SwiperSlide } from "swiper/react";
+import Swiper from "swiper";
+import { cn } from "@/lib/utils";
+import "swiper/css";
+
 import StudyContainer from "./StudyContainer";
 
+import { IoChevronBackSharp } from "react-icons/io5";
+import { IoChevronForwardSharp } from "react-icons/io5";
+import { GoDot, GoDotFill } from "react-icons/go";
+
 const OnlineStudyList = () => {
-  const [sliderCnt, setSliderCnt] = useState<number | undefined>();
+  const [sliderCnt, setSliderCnt] = useState(1);
+  const [sliderIndex, setSliderIndex] = useState<number>();
+  const [pageIndex, setPageIndex] = useState<0 | 1 | 2>(0);
+
+  const swiperRef = useRef<Swiper>();
+  const pageRef = useRef<Swiper>();
+  const sliderContentsCnt = 10;
+
+  const handleSliderChange = () => {
+    const prevIndex = sliderIndex;
+    setSliderIndex(swiperRef.current?.activeIndex);
+    if (swiperRef.current?.activeIndex! - prevIndex! > 0) {
+      if (pageIndex !== 2) setPageIndex((prev) => (prev + 1) as 1 | 2);
+      if (pageIndex === 2) pageRef.current?.slideNext();
+    }
+    if (swiperRef.current?.activeIndex! - prevIndex! < 0) {
+      if (pageIndex !== 0) setPageIndex((prev) => (prev - 1) as 0 | 1);
+      if (pageIndex === 0) pageRef.current?.slidePrev();
+    }
+  };
+
+  const handleSlideLeft = () => {
+    swiperRef.current?.slidePrev();
+  };
+
+  const handleSlideRight = () => {
+    swiperRef.current?.slideNext();
+  };
 
   useEffect(() => {
     window.addEventListener("resize", () => {
@@ -23,27 +59,74 @@ const OnlineStudyList = () => {
   }, []);
   return (
     <div>
-      <div className="font-semibold my-2 text-xl">💻 온라인 회화 모임!</div>
-      <Swiper
-        modules={[Pagination, A11y]}
-        spaceBetween={10}
-        slidesPerView={sliderCnt}
-        pagination={{ clickable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}>
-        <SwiperSlide className="bg-red-50">
-          <StudyContainer />
-        </SwiperSlide>
-        <SwiperSlide className="bg-red-50">
-          <StudyContainer />
-        </SwiperSlide>
-        <SwiperSlide className="bg-red-50">
-          <StudyContainer />
-        </SwiperSlide>
-        <SwiperSlide className="bg-red-50">
-          <StudyContainer />
-        </SwiperSlide>
-      </Swiper>
+      <div className="flex justify-between font-semibold my-2 text-xl ">
+        <div>💻 온라인 회화 모임!</div>
+        <div className="flex gap-1 text-sm items-center">
+          <div
+            className={cn(
+              "rounded-full p-1 transition",
+              sliderIndex === 0
+                ? "text-neutral-400"
+                : "hover:bg-neutral-200 hover:cursor-pointer"
+            )}
+            onClick={handleSlideLeft}>
+            <IoChevronBackSharp size={25} />
+          </div>
+          <div
+            className={cn(
+              "rounded-full p-1 transition",
+              sliderIndex === sliderContentsCnt - sliderCnt
+                ? "text-neutral-400"
+                : "hover:bg-neutral-200 hover:cursor-pointer"
+            )}
+            onClick={handleSlideRight}>
+            <IoChevronForwardSharp size={25} />
+          </div>
+        </div>
+      </div>
+      <div>
+        <SwiperContianer
+          modules={[A11y]}
+          spaceBetween={10}
+          slidesPerView={sliderCnt}
+          onSlideChange={() => handleSliderChange()}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+            setSliderIndex(swiper.activeIndex);
+          }}>
+          {Array.from({ length: 10 }, (_, index) => (
+            <SwiperSlide key={index} className="bg-red-50">
+              <StudyContainer />
+            </SwiperSlide>
+          ))}
+        </SwiperContianer>
+        <div className="flex justify-center items-center">
+          <div className="w-[80px]">
+            <SwiperContianer
+              modules={[A11y]}
+              spaceBetween={0}
+              slidesPerView={5}
+              onSwiper={(swiper) => {
+                pageRef.current = swiper;
+              }}>
+              <SwiperSlide>
+                <div className="w-4 h-4" />
+              </SwiperSlide>
+              {Array.from(
+                { length: Math.ceil(sliderContentsCnt / sliderCnt) },
+                (_, index) => (
+                  <SwiperSlide key={index}>
+                    {index === sliderIndex ? <GoDotFill /> : <GoDot />}
+                  </SwiperSlide>
+                )
+              )}
+              <SwiperSlide>
+                <div className="w-4 h-4" />
+              </SwiperSlide>
+            </SwiperContianer>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/StudyList/StudyNearbyList.tsx
+++ components/StudyList/StudyNearbyList.tsx
@@ -1,17 +1,48 @@
 "use client";
-import { A11y } from "swiper/modules";
-import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
 import { useEffect, useRef, useState } from "react";
+
+import { A11y } from "swiper/modules";
+import { Swiper as SwiperContianer, SwiperSlide } from "swiper/react";
+import Swiper from "swiper";
+import { cn } from "@/lib/utils";
+import "swiper/css";
+
 import StudyContainer from "./StudyContainer";
 
 import { IoChevronBackSharp } from "react-icons/io5";
 import { IoChevronForwardSharp } from "react-icons/io5";
+import { GoDot, GoDotFill } from "react-icons/go";
 
 // sm 640 md 768 lg 1024 xl 1280
 const StudyNearbyList = () => {
-  const [sliderCnt, setSliderCnt] = useState<number | undefined>();
+  const [sliderCnt, setSliderCnt] = useState(1);
+  const [sliderIndex, setSliderIndex] = useState<number>();
+  const [pageIndex, setPageIndex] = useState<0 | 1 | 2>(0);
+
+  const swiperRef = useRef<Swiper>();
+  const pageRef = useRef<Swiper>();
+  const sliderContentsCnt = 10;
+
+  const handleSliderChange = () => {
+    const prevIndex = sliderIndex;
+    setSliderIndex(swiperRef.current?.activeIndex);
+    if (swiperRef.current?.activeIndex! - prevIndex! > 0) {
+      if (pageIndex !== 2) setPageIndex((prev) => (prev + 1) as 1 | 2);
+      if (pageIndex === 2) pageRef.current?.slideNext();
+    }
+    if (swiperRef.current?.activeIndex! - prevIndex! < 0) {
+      if (pageIndex !== 0) setPageIndex((prev) => (prev - 1) as 0 | 1);
+      if (pageIndex === 0) pageRef.current?.slidePrev();
+    }
+  };
+
+  const handleSlideLeft = () => {
+    swiperRef.current?.slidePrev();
+  };
+
+  const handleSlideRight = () => {
+    swiperRef.current?.slideNext();
+  };
 
   useEffect(() => {
     window.addEventListener("resize", () => {
@@ -32,34 +63,70 @@ const StudyNearbyList = () => {
       <div className="flex justify-between font-semibold my-2 text-xl ">
         <div>🚩 내 주변 회화 모임!</div>
         <div className="flex gap-1 text-sm items-center">
-          ellipsis
-          <div className=" hover:bg-neutral-100">
-            <IoChevronBackSharp />
+          <div
+            className={cn(
+              "rounded-full p-1 transition",
+              sliderIndex === 0
+                ? "text-neutral-400"
+                : "hover:bg-neutral-200 hover:cursor-pointer"
+            )}
+            onClick={handleSlideLeft}>
+            <IoChevronBackSharp size={25} />
           </div>
-          <div>
-            <IoChevronForwardSharp />
+          <div
+            className={cn(
+              "rounded-full p-1 transition",
+              sliderIndex === sliderContentsCnt - sliderCnt
+                ? "text-neutral-400"
+                : "hover:bg-neutral-200 hover:cursor-pointer"
+            )}
+            onClick={handleSlideRight}>
+            <IoChevronForwardSharp size={25} />
           </div>
         </div>
       </div>
-      <div className="flex items-center mb-8 gap-2">
-        <Swiper
+      <div>
+        <SwiperContianer
           modules={[A11y]}
           spaceBetween={10}
           slidesPerView={sliderCnt}
-          onSlideChange={() => console.log("slide change")}>
-          <SwiperSlide className="bg-red-50">
-            <StudyContainer />
-          </SwiperSlide>
-          <SwiperSlide className="bg-red-50">
-            <StudyContainer />
-          </SwiperSlide>
-          <SwiperSlide className="bg-red-50">
-            <StudyContainer />
-          </SwiperSlide>
-          <SwiperSlide className="bg-red-50">
-            <StudyContainer />
-          </SwiperSlide>
-        </Swiper>
+          onSlideChange={() => handleSliderChange()}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+            setSliderIndex(swiper.activeIndex);
+          }}>
+          {Array.from({ length: 10 }, (_, index) => (
+            <SwiperSlide key={index} className="bg-red-50">
+              <StudyContainer />
+            </SwiperSlide>
+          ))}
+        </SwiperContianer>
+        <div className="flex justify-center items-center">
+          <div className="w-[80px]">
+            <SwiperContianer
+              modules={[A11y]}
+              spaceBetween={0}
+              slidesPerView={5}
+              onSwiper={(swiper) => {
+                pageRef.current = swiper;
+              }}>
+              <SwiperSlide>
+                <div className="w-4 h-4" />
+              </SwiperSlide>
+              {Array.from(
+                { length: Math.ceil(sliderContentsCnt / sliderCnt) },
+                (_, index) => (
+                  <SwiperSlide key={index}>
+                    {index === sliderIndex ? <GoDotFill /> : <GoDot />}
+                  </SwiperSlide>
+                )
+              )}
+              <SwiperSlide>
+                <div className="w-4 h-4" />
+              </SwiperSlide>
+            </SwiperContianer>
+          </div>
+        </div>
       </div>
     </div>
   );
