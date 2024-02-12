d4d6b9d6fe94c4fa85ba1717079cd004437bc468
lurgi <wahoo9040@naver.com>
2024-02-12 21:49:24 +0900

    feat(sign-in): useForm setting

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "@/lib/utils";
+
+const formSchema = z.object({
+  email: z.string().min(1, { message: "이메일을 입력하세요." }).max(50),
+  password: z.string().min(1, { message: "비밀번호를 입력하세요." }).max(50),
+});
 
 const SignIn = () => {
   const outsideOuth = {
@@ -17,17 +29,52 @@ const SignIn = () => {
   const outsideOuthKey = Object.keys(outsideOuth) as Array<
     keyof typeof outsideOuth
   >;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (form: z.infer<typeof formSchema>) => {
+    // TODO: Handle form submit
+    console.log(form.email);
+    console.log(form.password);
+  };
+
   return (
     <div className="w-[400px] mt-5 font-semibold">
-      <form className="flex flex-col gap-3">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
         <div className="text-2xl flex justify-center">로그인</div>
-        <input
-          placeholder="이메일"
-          className="border rounded-md py-2 px-4"></input>
-        <input
-          type="password"
-          placeholder="비밀번호"
-          className="border rounded-md py-2 px-4"></input>
+        <div>
+          <input
+            {...register("email")}
+            placeholder="이메일"
+            className={cn(
+              "border-2 rounded-md py-2 px-4 w-full outline-none",
+              errors.email ? "border-red-500 " : ""
+            )}
+          />
+          {errors.email ? (
+            <div className="text-red-600 mt-1">{errors.email.message}</div>
+          ) : null}
+        </div>
+        <div>
+          <input
+            {...register("password")}
+            type="password"
+            placeholder="비밀번호"
+            className={cn(
+              "border-2 rounded-md py-2 px-4 w-full outline-none",
+              errors.email ? "border-red-500" : ""
+            )}
+          />
+          {errors.password ? (
+            <div className="text-red-600 mt-1">{errors.password.message}</div>
+          ) : null}
+        </div>
         <button
           type="submit"
           className="w-full rounded-md py-2 px-4 bg-green-200 hover:bg-green-300 transition">
